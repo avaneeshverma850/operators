370da677727cbfbbb8b0f549a564c4059db83d2d
Avaneesh verma <124239728+avaneeshverma850@users.noreply.github.com>
2024-01-30 01:02:02 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,31 @@
+#problem 1
+x=2534
+print(x//10)
+#problem 2
+x=2089
+print(x%10)
+#swap of two numbers
+"""x=int(input("enter number:"))
+y=int(input("enter number:"))
+x=x+y
+y=x-y
+x=x-y
+print("After Swapping x is:",x)
+print("After Swapping y is:",y)
+#prpblem 4
+x=int(input("please enter number:"))
+x=x//10
+x=x//10
+print("First digit is:",x)
+#problem 5
+x=int(input("Enter number:"))
+x=x//10
+x=x%10
+print("Middle Digit is:",x)"""
+#prolem 6
+print(True or False)
+#problem 6
+print("Red" and "white")
+print(1 and 4)
+#problem 7
+print(5>10<5)
